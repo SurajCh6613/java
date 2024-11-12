629fd86b711eed6fe05492bae759b433bfae67e2
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-11-12 18:12:48 +0530

    Object and Class

---
+++ OOPs/Object_Class.java
@@ -0,0 +1,23 @@
+// Object
+    // 1. Object are real world entities.
+    // 2. Object must have 2 things -> Properties(State) and Behaviour(Function)
+
+
+// Class
+    // Class is a blueprint of an Object
+    // Class contains data variables and data methods
+    // Data methods are function which work on data variables
+
+// Class Creation
+public class Object_Class {
+    int age;
+    String name;
+
+
+    // Main method
+    public static void main(String[] args) {
+        
+        // Object creation of Object_Class
+        Object_Class ob = new Object_Class();
+    }
+}

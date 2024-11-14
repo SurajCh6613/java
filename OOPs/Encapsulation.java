117a04541531169b77ce6b83be052139af26084f
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2024-11-14 14:10:26 +0530

    Encapsulation

---
+++ OOPs/Encapsulation.java
@@ -0,0 +1,31 @@
+public class Encapsulation {
+    // Creating a class for Encapsulation
+    public static class Dog {
+        String color;
+        private int id = 102;
+
+        void setColor(String color){
+            this.color = color;
+        }
+        public void setId(int id) {
+            this.id = id;
+        }
+        int getId(){
+            return id;
+        }
+        String getColor(){
+            return color;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Creating Dog Object
+        Dog d1 = new Dog();
+        d1.setColor("black");
+        System.out.println(d1.getColor());
+        d1.setColor("blue");
+        System.out.println(d1.getColor());
+        d1.setId(103);
+        System.out.println(d1.getId());
+    }
+}
